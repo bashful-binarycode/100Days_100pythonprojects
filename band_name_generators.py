a0c181e06eeb63406d350b6ad1d74c3b0cec2fcf
Harsh Sharma <65808871+bashful-binarycode@users.noreply.github.com>
2022-04-04 14:57:58 +0530

    Delete band_name_generators.py

--- band_name_generators.py
+++
@@ -1,6 +0,0 @@
-#generates band names
-def generators(first_name = input("What's the name of the city you were born in?\n"), last_name = input("What's the name of your pet?\n")):
-    print('Welcome to the band name generators!')
-    print(first_name, last_name)
-
-generators()
