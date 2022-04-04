5e0e1ac2c7973f307c9babaa8cd7a3128855b041
Harsh Sharma <65808871+bashful-binarycode@users.noreply.github.com>
2022-04-05 01:48:15 +0530

    Update tipcalculator.py

--- Day2/tipcalculator.py
+++ Day2/tipcalculator.py
@@ -1,6 +1,6 @@
 def tips(amount: int, no_of_people: int, tip_percentage: int):
     print("Welcome to the tip calculator!")
     per_person = round((amount+((tip_percentage/100)*amount))/no_of_people, 2)
-    print(f"Your bill is {amount} and with tip percentage of {tip_percentage}%  each person has to pay {per_person}")
+    print(f"Your bill is {amount} and with tip percentage of {tip_percentage}%  each person has to pay {"{:.2f}".format(per_person)}")
 
 tips(500, 6, 12)
